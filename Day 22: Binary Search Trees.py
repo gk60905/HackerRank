a61be85a37bf6387f929af20e63fd660cbb60111
Rohit Swami <rowhitswami1@gmail.com>
2019-04-19 21:53:12 +0530

    Day 3

---
+++ Day 22: Binary Search Trees.py
@@ -0,0 +1,36 @@
+# Link : https://www.hackerrank.com/challenges/30-binary-search-trees/problem
+
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+    def getHeight(self,root):
+        #Write your code here
+        if root is None or (root.right is None and root.left is None):
+            return 0
+        else:
+            lHeight = self.getHeight(root.left)
+            rHeight = self.getHeight(root.right)
+            return max(lHeight, rHeight) + 1
+
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+height=myTree.getHeight(root)
+print(height)       
